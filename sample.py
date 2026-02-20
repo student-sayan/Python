4d7f1ca79190413b9fc6c46ff804361e1c2c603a
student-sayan <sayanroy702388@gmail.com>
2026-02-20 15:04:02 +0000

    Intro of data types

---
+++ sample.py
@@ -0,0 +1,20 @@
+#print("Hello")
+#print('My name is Sayan Roy')
+
+#Variable declaration
+name = "Sayan Roy"
+age = 20
+height = 7.5
+print(name,age,height)
+
+#Data types
+a= 10
+b= 20.60
+c= 3+4j
+d= "Name"
+print(type(a),type(b),type(c),type(d))
+
+#list
+My_list= ["Car","Bike","Cycle"]
+print(My_list)
+
